444cc22a9dc7691b6fe75106fd9adbd7ebef9e59
suryadevelope <suryaprakashlokula@gmail.com>
2023-11-26 08:13:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from multiapp import MultiApp
+import demo
+
+app = MultiApp()
+st.set_page_config(page_title="AttnGAN", initial_sidebar_state="auto")
+
+st.sidebar.title("Navigation")
+# Add all application here
+
+app.add_app("Demo", demo.demo_gan)
+# app.add_app("AttnGAN Explanation", demo.attngan_explained)
+
+
+# The main app
+app.run()
